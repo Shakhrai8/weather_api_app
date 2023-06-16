84fdb47af164c78c51f4cc4e66181d55af38c8af
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-16 12:29:22 +0100

    reworked the tests to handle the open hanles

--- weather_mockings.test.js
+++ weather_mockings.test.js
@@ -2,38 +2,42 @@ const Weather = require("./weather");
 const WeatherUI = require("./weather_ui");
 
 describe("Weather", () => {
+  let weather;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    const mockWeatherClass = {
+      fetchWeatherData: jest.fn().mockResolvedValue({
+        name: "London",
+        weather: "Sunny",
+        temperature: 25,
+        feels_like: 28,
+        humidity: 60,
+      }),
+    };
+    weather = new Weather(mockWeatherClass);
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
   describe("load method", () => {
-    it("should set current_data correctly", () => {
-      const mockWeatherClass = {
-        // creating double for mocking with built in methods from jest
-        fetchWeatherData: jest.fn().mockResolvedValue({
-          name: "London",
-          weather: "Sunny",
-          temperature: 25,
-          feels_like: 28,
-          humidity: 60,
-        }),
-      };
-
-      const weather = new Weather(mockWeatherClass);
+    it("should set current_data correctly", async () => {
       const city = "London";
-
-      return weather.load(city).then(() => {
-        expect(weather.current_data).toEqual({
-          city_name: "London",
-          weather: "Sunny",
-          temperature: 25,
-          feels_like: 28,
-          humidity: 60,
-        });
+      await weather.load(city);
+      expect(weather.current_data).toEqual({
+        city_name: "London",
+        weather: "Sunny",
+        temperature: 25,
+        feels_like: 28,
+        humidity: 60,
       });
     });
   });
 
   describe("getWeatherData method", () => {
     it("should log weather data to the console", () => {
-      const weather = new Weather();
-
       weather.current_data = {
         city_name: "London",
         weather: "Sunny",
@@ -50,15 +54,12 @@ describe("Weather", () => {
       expect(consoleLogSpy).toHaveBeenCalledWith("Temperature: 25");
       expect(consoleLogSpy).toHaveBeenCalledWith("Feels like: 28");
       expect(consoleLogSpy).toHaveBeenCalledWith("Humidity: 60");
-      // The original console.log function is replaced by the spy. After we have
-      // finished using the spy and performed any necessary assertions or test logic,
-      // we call consoleLogSpy.mockRestore() to restore the original console.log function.
       consoleLogSpy.mockRestore();
     });
   });
 
   describe("compareWith method", () => {
-    it("should compare temperatures and log the result", () => {
+    it("should compare temperatures and log the result", async () => {
       const mockWeatherClass = {
         fetchWeatherData: jest.fn().mockResolvedValue({
           name: "Paris",
@@ -66,7 +67,7 @@ describe("Weather", () => {
         }),
       };
 
-      const weather = new Weather(mockWeatherClass);
+      weather = new Weather(mockWeatherClass);
       weather.current_data = {
         city_name: "London",
         temperature: 25,
@@ -74,17 +75,15 @@ describe("Weather", () => {
 
       const consoleLogSpy = jest.spyOn(console, "log");
 
-      return weather.compareWith("Paris").then(() => {
-        expect(consoleLogSpy).toHaveBeenCalledWith(
-          "Paris is warmer than London"
-        );
-        consoleLogSpy.mockRestore();
-      });
+      await weather.compareWith("Paris");
+
+      expect(consoleLogSpy).toHaveBeenCalledWith("Paris is warmer than London");
+      consoleLogSpy.mockRestore();
     });
   });
 
   describe("WeatherUI", () => {
-    test("should display the weather information in the correct format", () => {
+    it("should display the weather information in the correct format", () => {
       const weatherMock = {
         current_data: {
           city_name: "London",
@@ -97,10 +96,7 @@ describe("Weather", () => {
 
       const consoleLogSpy = jest.spyOn(console, "log");
 
-      // Create WeatherUI instance with the weather mock
       const weatherUI = new WeatherUI(weatherMock);
-
-      // Invoke the method
       weatherUI.displayWeather();
 
       expect(consoleLogSpy).toHaveBeenCalledWith(`
@@ -111,7 +107,6 @@ describe("Weather", () => {
       Humidity:     64%
       `);
 
-      // Restore the original console.log implementation
       consoleLogSpy.mockRestore();
     });
   });
